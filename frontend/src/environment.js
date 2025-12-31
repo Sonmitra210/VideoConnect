78330b4c3608a15b0043ae999c7d0ba7695aeacb
Sonmitra <sonmitra.seth@gmail.com>
2025-12-31 22:29:18 +0530

    new changes added

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,5 +1,5 @@
 let IS_PROD = true;
 
-const server= IS_PROD? "https://videoconnectbackend.onrender.com/":"http://localhost:8000";
+const server= IS_PROD? "https://videoconnectbackend-abf9.onrender.com":"http://localhost:8000";
 
 export default server;
